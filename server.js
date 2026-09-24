0a99c95178149b997cce022d2f32357ed0d69747
johnstavola <134661901+johnstavola@users.noreply.github.com>
2026-09-24 18:40:05 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use("/api", require("./routes/auth"));
 app.use("/api", require("./routes/workouts"));
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+// REQUIRED for Render:
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
